66add42fa3bd4196fe96eb6ef58b051b56c022f9
testuser <testuser@test.com>
2023-05-17 19:38:15 +0900

    test quiz 01

--- src/day01/TestClass01.java
+++ src/day01/TestClass01.java
@@ -1,5 +1,21 @@
 package day01;
 
 public class TestClass01 {
-
+	public static void main(String[] args) {
+		int hong, go, kim;
+		hong = 20000;
+		go = 30000; 
+		kim = 50000;
+		int sum1 = hong + go + kim;
+		
+		System.out.println("========================================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("========================================");
+		System.out.println("홍길동\t\""+15+"\"\t"+3672+"-"+1234+"\t\\"+hong);
+		System.out.println("고길동\t\""+15+"\"\t"+2238+"-"+1234+"\t\\"+go);
+		System.out.println("김말이\t\""+15+"\"\t"+1234+"-"+1234+"\t\\"+kim);
+		System.out.println("----------------------------------------");
+		System.out.println("총합계\t\t\t\t\\"+sum1);
+		System.out.println("----------------------------------------");
+	}
 }
